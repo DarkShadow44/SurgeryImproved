3fdcd997ce6c524a07253007711f8e5a3d67d5e0
Fabian Maurer <dark.shadow4@web.de>
2024-04-27 01:33:41 +0200

    First version

---
+++ Source/SurgeryImproved/HealthUtility_GiveRandomSurgeryInjuries_Patch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace KeepBlueprintSettings
+{
+    [HarmonyPatch(typeof(HealthUtility))]
+    [HarmonyPatch(nameof(HealthUtility.GiveRandomSurgeryInjuries))]
+    static class HealthUtility_GiveRandomSurgeryInjuries_Patch
+    {
+        static bool Prefix(Pawn p, int totalDamage, BodyPartRecord operatedPart)
+        {
+            if (operatedPart == null)
+            {
+                operatedPart = p.RaceProps.body.corePart;
+            }
+            while (p.health.hediffSet.GetPartHealth(operatedPart) <= 0)
+            {
+                operatedPart = operatedPart.parent ?? p.RaceProps.body.corePart;
+            }
+
+            if (p.health.WouldLosePartAfterAddingHediff(HediffDefOf.Cut, operatedPart, totalDamage))
+            {
+                float partHealth = p.health.hediffSet.GetPartHealth(operatedPart);
+                if (partHealth / operatedPart.def.GetMaxHealth(p) >= 0.75)
+                {
+                    Random random = new Random();
+                    totalDamage = random.Next((int)(partHealth * 0.7), (int)partHealth);
+                }
+            }
+            DamageDef def = Rand.Element(DamageDefOf.Cut, DamageDefOf.Scratch, DamageDefOf.Stab, DamageDefOf.Crush);
+            DamageInfo dinfo = new DamageInfo(def, totalDamage, 0f, -1f, null, operatedPart);
+            dinfo.SetIgnoreArmor(true);
+            dinfo.SetIgnoreInstantKillProtection(true);
+            p.TakeDamage(dinfo);
+            return false;
+        }
+    }
+}
---
+++ Source/SurgeryImproved/SurgeryImproved.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace KeepBlueprintSettings
+{
+    [StaticConstructorOnStartup]
+    public class SurgeryImproved
+    {
+        static SurgeryImproved()
+        {
+            var harmony = new Harmony("DarkShadow44.SurgeryImproved.Harmony");
+            harmony.PatchAll();
+        }
+    }
+}
