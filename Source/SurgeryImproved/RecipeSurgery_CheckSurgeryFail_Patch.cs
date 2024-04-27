ab36f5a1b422b4221f80213f8db5d63780908013
Fabian Maurer <dark.shadow4@web.de>
2024-04-27 02:23:42 +0200

    Rework to give back items

---
+++ Source/SurgeryImproved/RecipeSurgery_CheckSurgeryFail_Patch.cs
@@ -0,0 +1,55 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+namespace SurgeryImproved
+{
+    [HarmonyPatch(typeof(Recipe_Surgery))]
+    [HarmonyPatch("CheckSurgeryFail")]
+    internal class RecipeSurgery_CheckSurgeryFail_Patch
+    {
+        public static void DropIngredients(Pawn surgeon, List<Thing> ingredients)
+        {
+            foreach (var ingredient in ingredients)
+            {
+                if (ingredient.def.isTechHediff)
+                {
+                    var health = ingredient.HitPoints - (int)(ingredient.MaxHitPoints * Math.Min(0.25, Rand.Value * 0.5));
+                    if (health > 0)
+                    {
+                        var item = GenSpawn.Spawn(ingredient.def, surgeon.Position, surgeon.Map);
+                        item.HitPoints = health;
+                    }
+                }
+            }
+        }
+
+        static bool Prefix(ref bool __result, Recipe_Surgery __instance, Pawn surgeon, Pawn patient, List<Thing> ingredients, BodyPartRecord part, Bill bill)
+        {
+            var recipe = __instance.recipe;
+
+            SurgeryOutcome outcome = recipe.surgeryOutcomeEffect?.GetOutcome(recipe, surgeon, patient, ingredients, part, bill);
+            if (outcome != null && outcome.failure)
+            {
+                if (recipe.addsHediffOnFailure != null)
+                {
+                    patient.health.AddHediff(recipe.addsHediffOnFailure, part);
+                }
+
+                // Recreate items
+                if (outcome.applyEffectsToPart)
+                {
+                    DropIngredients(surgeon, ingredients);
+                }
+
+                __result = true;
+                return false;
+            }
+
+            __result = false;
+            return false;
+        }
+    }
+}
---
+++ Source/SurgeryImproved/SurgeryOutcome_ApplyDamage_Patch.cs
@@ -0,0 +1,60 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace KeepBlueprintSettings
+{
+    [HarmonyPatch(typeof(SurgeryOutcome))]
+    [HarmonyPatch("ApplyDamage")]
+    static class SurgeryOutcome_ApplyDamage_Patch
+    {
+        static void ApplyDamage(Pawn patient, BodyPartRecord part, int totalDamage, bool preventDestruction)
+        {
+            if (part == null)
+            {
+                part = patient.RaceProps.body.corePart;
+            }
+            while (patient.health.hediffSet.GetPartHealth(part) <= 0)
+            {
+                part = part.parent ?? patient.RaceProps.body.corePart;
+            }
+
+            if (patient.health.WouldLosePartAfterAddingHediff(HediffDefOf.Cut, part, totalDamage))
+            {
+                float partHealth = patient.health.hediffSet.GetPartHealth(part);
+                if (partHealth / part.def.GetMaxHealth(patient) >= 0.7 && preventDestruction)
+                {
+                    Random random = new Random();
+                    var min = (int)(partHealth * 0.7);
+                    if (min == (int)partHealth)
+                    {
+                        return;
+                    }
+                    totalDamage = random.Next(min, (int)partHealth);
+                }
+            }
+            DamageDef def = Rand.Element(DamageDefOf.Cut, DamageDefOf.Scratch, DamageDefOf.Stab, DamageDefOf.Crush);
+            DamageInfo dinfo = new DamageInfo(def, totalDamage, 0f, -1f, null, part);
+            dinfo.SetIgnoreArmor(true);
+            dinfo.SetIgnoreInstantKillProtection(true);
+            dinfo.SetAllowDamagePropagation(false);
+            patient.TakeDamage(dinfo);
+        }
+
+        static bool Prefix(SurgeryOutcome __instance, Pawn patient, BodyPartRecord part)
+        {
+            ApplyDamage(patient, part, __instance.totalDamage, __instance.applyEffectsToPart);
+            if (__instance.totalDamage > 10)
+            {
+                part = part.parent;
+                while (part != null && Rand.Value < 0.25)
+                {
+                    ApplyDamage(patient, part, __instance.totalDamage, true);
+                    part = part.parent;
+                }
+            }
+            return false;
+        }
+    }
+}
